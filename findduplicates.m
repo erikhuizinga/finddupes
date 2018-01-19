2f70e7efbd3f0499c8322918b39e8810e7d5c37a
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2018-01-19 13:52:42 +0100

    Extend documentation, support new syntax

--- findduplicates.m
+++ findduplicates.m
@@ -1,11 +1,11 @@
-function [i1, i2, C, ia, ic] = findduplicates(A, varargin)
 %FINDDUPLICATES Find linear indices of duplicates in input.
 %   This function accepts the same input arguments as the unique function,
 %   which is the basis for this function. FINDDUPLICATES has two output
 %   arguments, i1 and i2.
 %
 %   Syntax
-%   [i1, i2] = FINDDUPLICATES(A, __) finds the linear indices of duplicates
+%
+%   [i1, i2] = FINDDUPLICATES(A) finds the linear indices of duplicates
 %   in input A. The accepted input arguments are the same as accepted by
 %   the unique function. The first output, i1, contains the linear indices
 %   of the first duplicate elements into A that correspond to the
@@ -13,11 +13,40 @@ function [i1, i2, C, ia, ic] = findduplicates(A, varargin)
 %   index multiple times if an element exists more than twice in A. If no
 %   duplicates exist, i1 and i2 are returned empty.
 %
+%   [i1, i2] = FINDDUPLICATES(A, setOrder) supports the 'sorted' (default)
+%   or 'stable' flags of the unique function, but have no effect to find
+%   duplicates.
+%
+%   [i1, i2] = FINDDUPLICATES(A, occurence) specifies which duplicate to
+%   consider as the first, occurence can be 'first' (default) or 'last'.
+%
 %   Examples
-%   Run findduplicatesdemo for a demonstration of FINDDUPLICATES. See the
-%   contents of findduplicatesdemo.m for details.
+%
+%   Find first indices of duplicates and the other duplicates' indices:
+%
+%   A = [9, 2, 9, 5]
+%   [i1, i2] = findduplicates(A)
+%
+%   Result
+%
+%   A =
+%        9     2     9     5
+%
+%   i1 =
+%        1
+%
+%   i2 =
+%        3
+%
+%   This indicates that the first element in A has a duplicate at the
+%   third element.
+%
+%   Run findduplicatesdemo for more examples of FINDDUPLICATES based on
+%   the examples of the unique function. See the contents of
+%   findduplicatesdemo.m for details.
 %
 %   Notes
+%
 %    - The 'stable' flag of the unique function has no functionality in
 %    FINDDUPLICATES, but is allowed for compatibility.
 %    - NaN are considered as distinct values by the unique function, thus
@@ -33,8 +62,8 @@ function [i1, i2, C, ia, ic] = findduplicates(A, varargin)
 %   This program is free software: you can redistribute it and/or modify
 %   it under the terms of the GNU General Public License as published by
 %   the Free Software Foundation, either version 3 of the License, or
-%   any later version.
-%   
+%   any ater version.
+%
 %   This program is distributed in the hope that it will be useful,
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of
 %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -43,14 +72,18 @@ function [i1, i2, C, ia, ic] = findduplicates(A, varargin)
 %   You should have received a copy of the GNU General Public License
 %   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-[C, ia, ic] = unique(A, varargin{:}); % indices of unique values
-if istable(A)
-    i = 1 : height(A);
-elseif nargin > 1
-    if any(strcmpi(varargin{:}, 'rows'))
-        i = 1 : size(A, 1);
+function [i1, i2, C, ia, ic] = findduplicates(A, varargin)
+    [C, ia, ic] = unique(A, varargin{:}); % indices of unique values
+    
+    if istable(A)
+        i = 1 : height(A);
+    elseif nargin > 1
+        if any(strcmpi(varargin, 'rows'))
+            i = 1 : size(A, 1);
+        end
     end
+    if ~exist('i', 'var'), i = 1 : numel(A); end
+    
+    i2 = setdiff(i(:), ia); % indices of duplicates corresponding to i1
+    i1 = ia(ic(i2)); % indices of duplicates found first by unique
 end
-if ~exist('i', 'var'), i = 1 : numel(A); end
-i2 = setdiff(i(:), ia); % indices of duplicates corresponding to i1
-i1 = ia(ic(i2)); % indices of duplicates found first by unique
